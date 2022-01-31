20f7902224f9656a727be02f73031095993cc0a2
yperunov <perunovyuri@gmail.com>
2022-01-31 23:08:44 +0300

    Initial commit

---
+++ src/main/java/dev/ji/employeenewnew/EmployeenewnewApplication.java
@@ -0,0 +1,13 @@
+package dev.ji.employeenewnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeenewnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeenewnewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ji/employeenewnew/EmployeenewnewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ji.employeenewnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeenewnewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
